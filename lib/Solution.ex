30160183d5dcefb855aed13001f27027a2cc616a
kahono922 <kahonojackson@gmail.com>
2023-05-08 16:36:34 +0300

    Start leetcode with elixir

---
+++ lib/Solution.ex
@@ -0,0 +1,28 @@
+defmodule Solution do
+  @spec diagonal_sum(mat :: [[integer]]) :: integer
+  def diagonal_sum(mat) do
+    # calculate sum of diagonal elements
+    # return sum
+
+    """
+     sum_1 = Enum.reduce(1..length(mat), 0, fn i, acc ->
+       acc + Enum.at(Enum.at(mat, i - 1), i - 1)
+     end)
+
+     sum_2 = Enum.reduce(0..(length(mat) - 1), 0, fn i, acc ->
+       acc + Enum.at(Enum.at(mat, length(mat) - i - 1), i)
+     end)
+
+      """
+
+      sum_1_2 = Enum.reduce(0..length(mat) - 1, 0, fn i, acc ->
+        acc + Enum.at(Enum.at(mat, i), i) + Enum.at(Enum.at(mat, length(mat) - i - 1), i)
+      end)
+
+      if rem(length(mat), 2) == 0 do
+        sum_1_2
+      else
+        sum_1_2 - Enum.at(Enum.at(mat, div(length(mat), 2)), div(length(mat), 2))
+      end
+  end
+end
