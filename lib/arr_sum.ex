855a7b44835fb833a7a4f8e7719eab6e4b013b72
kahono922 <kahonojackson@gmail.com>
2023-05-15 14:55:02 +0300

    Update length of array

--- lib/arr_sum.ex
+++ lib/arr_sum.ex
@@ -1,9 +1,14 @@
 defmodule Sum do
-
+  """
   def sum_arr(arr,count) do
     case arr do
       [] -> count
       [_ | tail] -> sum_arr(tail, count + 1)
     end
   end
+  """
+
+  def sum(0), do: 0
+  def sum(n), do: n + sum(n - 1)
+
 end
